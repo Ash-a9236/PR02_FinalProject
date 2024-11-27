7f2243c46d9e718dece7cc47db9b9383f30092c5
Loading... <158498496+Ash-a9236@users.noreply.github.com>
2024-11-27 11:46:22 -0500

    base files

---
+++ src/main/java/ash_a9236/example/pr02_finalproject1/HelloController.java
@@ -0,0 +1,14 @@
+package ash_a9236.example.pr02_finalproject1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
