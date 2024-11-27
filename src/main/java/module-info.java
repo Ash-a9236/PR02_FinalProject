6cc9b71942d37252839a1218aa74e981208b785c
Loading... <158498496+Ash-a9236@users.noreply.github.com>
2024-11-27 11:35:58 -0500

    Adding base files

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module ash_a9236.example.pr02_finalproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens ash_a9236.example.pr02_finalproject to javafx.fxml;
+    exports ash_a9236.example.pr02_finalproject;
+}
